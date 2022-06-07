4b360eb9e3f41601aae1ee22a5442bc68e21ec11
Mauricio <Mauricio@192.168.100.4>
2022-06-07 16:09:48 -0500

    version 0

---
+++ cvmau2022/src/cvmau2022/Cvmau2022.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cvmau2022;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Cvmau2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("control versiones 2022 v1");
+    }
+    
+}
