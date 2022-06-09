51df3bb5daba6d990767a8b68dd75243a3e334e6
Mauricio <Mauricio@192.168.100.6>
2022-06-09 13:16:18 -0500

    ver1 desarrollador 1

--- branch1/src/branch1/Branch1.java
+++ branch1/src/branch1/Branch1.java
@@ -17,6 +17,8 @@ public class Branch1 {
     public static void main(String[] args) {
         System.out.println("version 0");
         System.out.println("Versión 1");
+        System.out.println("version wmrc");
+        System.out.println("version wmrc1");
     }
     
 }
